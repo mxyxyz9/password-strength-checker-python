7a204f5b36a78c91711bac4839acd8a273be6cdb
Rushil <121330302+mxyxyz9@users.noreply.github.com>
2025-06-20 19:14:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def strong_pass(Password):
     if re.match(r'([a-zA-Z0-9]).{8,}', Password) is not None:
         print('Strong Password')
     else:
-        print('Weak Password')
+        print('Weak Password change it or update it to be sronger')
 
 # Get password from user
 password = input("Enter your password: ")
